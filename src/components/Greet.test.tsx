bb1d7ae0526fb44b2896cedd8c816bb7edfc13bf
vishnuSekhar <vishnusekhar96@gmail.com>
2022-12-07 16:33:31 +0530

    Add coverage script

--- src/components/Greet.test.tsx
+++ src/components/Greet.test.tsx
@@ -8,17 +8,37 @@
  * 2. write code
  * 3. Refactoring
  */
-
+/**
+ * Naming convention
+ * .test.js, .test.tsx, .spec.js, .spec.tsx
+ * .js or .tsx within __tests__ folders
+ */
+//test <=>it, test.only <=> ffit, test.skip<=>xit
 import { render, screen } from "@testing-library/react";
 import Greet from "./greet/Greet";
+describe("Greet", () => {
+  test("Renders correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText("Hello");
+    expect(textElement).toBeInTheDocument();
+  });
+  describe("Nested", () => {
+    test("Renders with a name", () => {
+      render(<Greet name={"Vishnu"} />);
+      const textElement = screen.getByText("Hello Vishnu");
+      expect(textElement).toBeInTheDocument();
+    });
+  });
+});
+//test.only => jest only pickups this test to run
+//test.skip => just skips this test while running
 
-test("Greet renders correctly", () => {
-  render(<Greet />);
-  const textElement = screen.getByText("Hello");
-  expect(textElement).toBeInTheDocument();
-});
-test("Greet renders with a name", () => {
-  render(<Greet name={"Vishnu"} />);
-  const textElement = screen.getByText("Hello Vishnu");
-  expect(textElement).toBeInTheDocument();
-});
+//describe method
+//able to use 'only' ans 'skip'
+//can nest describe blocks
+
+//Code coverage: A metric that can help you understand how much of your software code is tested.
+//a. statement coverage
+//b. branches coverage
+//c. function coverage
+//d. line coverage
